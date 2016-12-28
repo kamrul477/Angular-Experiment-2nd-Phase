0434745713989af91cf94124a18468cb459707bd
Kamrul <Kamrul>
2016-12-28 14:09:54 +0600

    2nd commit

---
+++ Angular Solution/FrontEnd.ProductMgnt/app/models/product.model.ts
@@ -0,0 +1,22 @@
+
+
+export interface IProduct {
+
+        productId: number;
+        productName: string;
+        productCode: number;
+        releaseDate: Date;
+        description: string;
+        price: number;
+        starRating: number;
+        imageUrl: string;
+    //           "productId": 1,
+    //"productName": "Leaf Rake",
+    //"productCode": "GDN-0011",
+    //"releaseDate": "March 19, 2016",
+    //"description": "Leaf rake with 48-inch wooden handle.",
+    //"price": 19.95,
+    //"starRating": 3.2,
+    //"imageUrl": "http://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png"
+
+}
---
+++ Angular Solution/FrontEnd.ProductMgnt/app/product/productList.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { IProduct } from '../models/product.model';
+import {ProductService} from '../services/product.service';
+
+@Component({
+    selector: 'pm-products',
+    templateUrl: 'app/product/product-list.component.html'
+})
+
+export class ProductListComponent implements OnInit {
+    pageTitle: string = 'Product List';
+    imageWidth: number = 50;
+    imageMargin: number = 2;
+    showImage: boolean = false;
+    listFilter: string = 'cart';
+    errorMessage :string;
+    products: IProduct[]; 
+    private _productService: ProductService;
+
+    constractor(productService: ProductService) {
+       this._productService = productService;
+    }
+    ngOnInit(): void {
+        this._productService.getProducts()
+            .subscribe(products => this.products = products,
+                error => this.errorMessage = <any>error);
+    }
+
+
+    toggleImage(): void {
+        this.showImage = !this.showImage;
+    }
+}
